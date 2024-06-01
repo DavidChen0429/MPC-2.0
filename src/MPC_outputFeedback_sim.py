2cd5917b99af8a41208d884d3aa43ae5c2171dec
lkehler <Leon.Kehler@outlook.com>
2024-06-01 10:39:53 +0200

    changed lqr function signature

--- src/MPC_outputFeedback_sim.py
+++ src/MPC_outputFeedback_sim.py
@@ -3,13 +3,14 @@ from planner import trajectory_generation
 import matplotlib.pyplot as plt
 import numpy as np
 
-drone_model = drone_dynamics.Quadrotor()
-
 # define simulation parameters
 dt = 0.1 
 t_final = 20.0 # seconds
 time_steps = int(t_final / dt)
 time = np.linspace(0, t_final, time_steps)
+Q = np.eye(12)
+parms = {"Q": Q, "R": np.eye(4), "N": 10, "Qf": Q, "dynamic": True}
+drone_model = drone_dynamics.Quadrotor(parms)
 
 # Augment the system with a constant disturbance term d
 d = [1]
